d40272ced321fa304149f9d26795517403565c5f
Hoang-Anh208 <nghoanganh01@gmail.com>
2024-07-27 13:54:33 +0700

    Update Led_Matrix.c

--- Lesson 3 - goto_setjmp.h/goto/Led_Matrix.c
+++ Lesson 3 - goto_setjmp.h/goto/Led_Matrix.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
+#include <time.h>
 
-void delay(){
-    double start;
-    while (start < 60000000) start++;    
+// hàm delay theo giây, ví dụ: delay(2000) -> 2s
+void delay(unsigned int milliseconds) {
+    unsigned int start = clock();
+    while (clock() - start < milliseconds);
 }
 
-char first_sentence[]  = "HELLO";
+char first_sentence[]  = "HI";
 char second_sentence[] = "FASHION";
 char third_sentence[]  = "SUITABLE";
 
 int letter_H[8][8] = {  
-    {1,0,0,0,1,0,0,0},
-    {1,0,0,0,1,0,0,0},
-    {1,0,0,0,1,0,0,0},
-    {1,1,1,1,1,0,0,0},
-    {1,0,0,0,1,0,0,0},
-    {1,0,0,0,1,0,0,0},
-    {1,0,0,0,1,0,0,0},
-    {1,0,0,0,1,0,0,0},  
+    {1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,1},
+    {1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1},
+    {1,0,0,0,0,0,0,1},  
+};
+
+int letter_I[8][8] = {  
+    {1,1,1,1,1,1,1,1},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {0,0,0,0,1,0,0,0},
+    {1,1,1,1,1,1,1,1},  
 };
 
 int letter_F[8][8] = {  
-    {1,1,1,1,1,0,0,0},
+    {1,1,1,1,1,1,1,1},
     {1,0,0,0,0,0,0,0},
     {1,0,0,0,0,0,0,0},
-    {1,1,1,1,1,0,0,0},
+    {1,1,1,1,1,1,1,1},
     {1,0,0,0,0,0,0,0},
     {1,0,0,0,0,0,0,0},
     {1,0,0,0,0,0,0,0},
@@ -32,106 +45,105 @@ int letter_F[8][8] = {
 };
 
 int letter_S[8][8] = {  
-    {0,1,1,1,1,0,0,0},
+    {0,1,1,1,1,1,1,1},
     {1,0,0,0,0,0,0,0},
     {1,0,0,0,0,0,0,0},
-    {0,1,1,1,0,0,0,0},
-    {0,0,0,0,1,0,0,0},
-    {0,0,0,0,1,0,0,0},
-    {0,0,0,0,1,0,0,0},
-    {1,1,1,1,0,0,0,0},  
+    {0,1,1,1,1,1,1,0},
+    {0,0,0,0,0,0,0,1},
+    {0,0,0,0,0,0,0,1},
+    {0,0,0,0,0,0,0,1},
+    {1,1,1,1,1,1,1,0},  
 };
 
+// mô phỏng nút nhấn bấm dừng chương trình
 int button = 0;
 
+// khởi tạo kiểu liệt kê đại diện cho 3 câu cần hiển thị
 typedef enum{
-    FIRST,
-    SECOND,
-    THIRD
-} Sentence_t;
+	FIRST,
+	SECOND,
+	THIRD
+} Sentence;
 
-int main(){
+// hàm in ký tự ra led ma trận 8x8
+void print_char(const int matrix[8][8]){
+	for (int i=0; i<8; i++){
+		for (int j=0; j<8; j++){
+			if (matrix[i][j] == 1){
+				printf("*");
+			}
+			else{
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+}
 
-    Sentence_t sentence = FIRST;
+int main(int argc, char const *argv[]){
+	// Biến `sentence` được khởi tạo với giá trị `FIRST`, đại diện cho câu đầu tiên cần hiển thị.
+	Sentence sentence = FIRST;
 
-    while(1){
-        switch (sentence){
-        	case FIRST:
-            	for (int index=0; index<sizeof(first_sentence); index++){
-                	if (first_sentence[index] == 'H'){
-                		for (int i=0; i<8; i++){    
-                        		for (int j=0; j<8; j++){
-                            			if (letter_H[i][j] == 1){
-                                			printf("Turn on led at [%d][%d]\n", i,j);
-                                			if (button == 1) goto exit_loops;
-                            			}
-                        		}
-                        		// tat den
-                    		}
-                	}
-                	if (first_sentence[index] == 'E'){
-                    	// in ra chu e
-                	}
-            	}
-            	printf("first sentence is done\n");
-            	delay();
-            	goto logic;
+	while(1){
+		switch(sentence){
+			case FIRST:
+				for (int index=0; index<sizeof(first_sentence); index++){
+					if (first_sentence[index] == 'H'){
+						print_char(letter_H);
+					}
+					else if (first_sentence[index] == 'I'){
+						print_char(letter_I);
+					}
+					if (button == 1) goto exit_loops;
+				}
+				printf("First sentence is done\n");
+				printf("\n");
+				delay(2000);
+				goto logic;
 
 			case SECOND:
 				for (int index=0; index<sizeof(second_sentence); index++){
 					if (second_sentence[index] == 'F'){
-						for (int i=0; i<8; i++){    
-							for (int j=0; j<8; j++) {
-								if (letter_F[i][j] == 1) {
-									printf("Turn on led at [%d][%d]\n", i,j);
-									if (button == 1) goto exit_loops;
-								}
-							}
-							// tat den led
-						}
+						print_char(letter_F);
 					}
-					if (second_sentence[index] == 'A'){
-						// in ra chu A
+					else if (second_sentence[index] == 'A'){
+						// in ra ký tự 'A'
 					}
+					if (button == 1) goto exit_loops;
 				}
-				printf("second sentence is done\n");
-				delay();
+				printf("Second sentence is done\n");
+				printf("\n");
+				delay(2000);
 				goto logic;
 
 			case THIRD:
 				for (int index=0; index<sizeof(third_sentence); index++){
 					if (third_sentence[index] == 'S'){
-						for (int i=0; i<8; i++){    
-							for (int j=0; j<8; j++){
-								if (letter_S[i][j] == 1) {
-									printf("Turn on led at [%d][%d]\n", i,j);
-									if (button == 1) goto exit_loops;
-								}
-							}
-							// tat den led
-						}
+						print_char(letter_S);
 					}
-					if (third_sentence[index] == 'U'){
-						// in ra chu H
+					else if (third_sentence[index] == 'U'){
+						// in ra ký tự 'U'
 					}
+					if (button == 1) goto exit_loops;
 				}
-				printf("third sentence is done\n");
-				delay();
-				//button = 1;
+				printf("Third sentence is done\n");
+				printf("\n");
+				delay(2000);
 				goto logic;
-			}
+		}
 
-        logic:
-            if (sentence == FIRST) 		 sentence = SECOND;
-            else if (sentence == SECOND) sentence = THIRD;
-            else if (sentence == THIRD)  sentence = FIRST;
-            goto exit;
-            
-        exit_loops:
-            printf("Stop!\n");
-            break;
-        
-        exit:;
-    }
+		logic:
+			if (sentence == FIRST) 		 sentence = SECOND;
+			else if (sentence == SECOND) sentence = THIRD;
+			else if (sentence == THIRD)  sentence = FIRST;
+			goto exit;
+		
+		exit_loops:
+			printf("Stop\n");
+			break;
+		
+		exit:;
+	}
+
 	return 0;
 }
